17b5071386a875a64bb6e58ed2bc625f1e6246e9
arjunTltech <arjunntltechnologies@gmail.com>
2025-05-30 14:39:35 +0530

    resolved api route issue

--- src/api/api.js
+++ src/api/api.js
@@ -41,7 +41,7 @@ export const OrganizationApi = {
 
 
 export const faqsApi = {
-    getAllfaqs: () => axiosInstance.get("/faqs/get-faqs"),
+    getAllfaqs: () => axiosInstance.get("/faqs/get-page-faqs"),
   };
 export const HomefaqsApi = {
     getAllfaqs: () => axiosInstance.get("/faqs/get-home-faqs"),
